727b34586af56f78a9cc47385e98fbd33fbcda3d
abanerjee10 <aranyaban@yahoo.com>
2021-12-02 00:30:40 -0500

    Update tsp_main.py

--- code/tsp_main.py
+++ code/tsp_main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import networkx as nx
-import numpy as np
 from scipy.spatial import distance_matrix
 from optparse import OptionParser
 import GA
